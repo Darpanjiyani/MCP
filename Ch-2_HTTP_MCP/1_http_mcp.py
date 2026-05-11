390926e13c28adbf9cf2df406ae72a3de676dbd7
Darpan Jiyani <darpanjiyani11@gmail.com>
2026-05-11 01:21:02 -0700

    Working on Chapter 2

---
+++ Ch-2_HTTP_MCP/1_http_mcp.py
@@ -0,0 +1,24 @@
+from fastmcp import FastMCP
+
+# Create an instance of FastMCP
+mcp = FastMCP()
+
+@mcp.tool()
+def fetch():
+    '''Use this tool to Fetch data from a source.'''
+
+    # Simulate fetching data from a source
+    '''In a real application, this could be a database query, an API call, etc.'''
+    return {"data": "Hello, World!"}
+
+@mcp.tool()
+def process():
+    '''Use this tool to Process the fetched data.'''
+
+    # Simulate processing the data
+    '''In a real application, this could involve data transformation, analysis, etc.'''
+    return {"processed_data": "Data has been processed!"}
+
+if __name__ == "__main__":
+    # Run the MCP server using HTTP transport
+    mcp.run(transport="streamable-http", host="0.0.0.0", port=8050)
